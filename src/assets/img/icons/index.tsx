e173959e0f9e4af5fc118a04f4d45ca84dd92ecf
Wyero JR <kajuwyero@gmail.com>
2025-10-22 11:26:56 +0700

    update homepage

--- src/assets/img/icons/index.tsx
+++ src/assets/img/icons/index.tsx
@@ -1,3 +1,6 @@
 import malaysia from "./MY - Malaysia.svg";
+import singapore from "./SG - Singapore.svg";
+import thailand from "./TH - Thailand.svg";
+import southkorea from "./KR - Korea (South).svg";
 
-export { malaysia }
+export { malaysia, singapore, thailand, southkorea };
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { images, img1, img2, img3 } from '@/assets/img'
+import { images, images1, images2, images3, images4, img1, img2, img3, imgMarg1, imgMarg2, imgMarg3, imgMarg4, imgMarg5, imgMarg6, imgMarg7, tech1, tech2, tech3 } from '@/assets/img'
 import { Button } from '@/components/ui/button'
 import { AiOutlineRise } from 'react-icons/ai'
 import { FaArrowRightLong, FaQuoteRight, FaUsers } from 'react-icons/fa6'
@@ -7,7 +7,8 @@ import { IoExtensionPuzzleOutline, IoSearch } from 'react-icons/io5'
 import { MdWifiProtectedSetup } from 'react-icons/md'
 import { LiaHospitalAltSolid } from 'react-icons/lia'
 import { HiBuildingLibrary } from 'react-icons/hi2'
-import { malaysia } from '@/assets/img/icons'
+import { malaysia, singapore, southkorea, thailand } from '@/assets/img/icons'
+import Marquee from 'react-fast-marquee'
 
 const Home = () => {
   const keyChallenges = [
@@ -22,13 +23,28 @@ const Home = () => {
 
   const destinations = [
     { flag: malaysia, name: "malaysia", description: "Affordable, modern hospitals with English‑speaking staff. Popular for fertility, cardiology, health screenings, and holistic wellness.", image: images },
-    { flag: malaysia, name: "malaysia", description: "Affordable, modern hospitals with English‑speaking staff. Popular for fertility, cardiology, health screenings, and holistic wellness.", image: images },
-    { flag: malaysia, name: "malaysia", description: "Affordable, modern hospitals with English‑speaking staff. Popular for fertility, cardiology, health screenings, and holistic wellness.", image: images },
-    { flag: malaysia, name: "malaysia", description: "Affordable, modern hospitals with English‑speaking staff. Popular for fertility, cardiology, health screenings, and holistic wellness.", image: images },
-    { flag: malaysia, name: "malaysia", description: "Affordable, modern hospitals with English‑speaking staff. Popular for fertility, cardiology, health screenings, and holistic wellness.", image: images },
-    { flag: malaysia, name: "malaysia", description: "Affordable, modern hospitals with English‑speaking staff. Popular for fertility, cardiology, health screenings, and holistic wellness.", image: images },
+    { flag: singapore, name: "singapore", description: "Premium hub for complex specialty care. Internationally accredited hospitals excel in oncology, neurosurgery, and organ transplants with trusted outcomes.", image: images1 },
+    { flag: thailand, name: "thailand", description: "Renowned hospitals in Bangkok and Phuket offer quality care, from heart surgery to cosmetic procedures.", image: images2 },
+    { flag: southkorea, name: "south korea", description: "Global leader in cosmetic surgery, dermatology, and high‑tech treatments with excellent patient care.", image: images3 },
+    { flag: malaysia, name: "india", description: "Trusted for advanced cardiac, orthopedic, oncology, and transplant care at a fraction of Western costs. Specialists with strong international training.", image: images4 },
+    { flag: malaysia, name: "turkey", description: "Gateway for Europe and Middle East patients. Competitive for hair restoration, fertility, ophthalmology, and cosmetic surgery.", image: images },
   ]
 
+  const technology = [
+    { title: "Access is borderless.", img: tech1 },
+    { title: "Care is transparent.", img: tech2 },
+    { title: "Patients are empowered.", img: tech3 },
+  ]
+
+  const imgMargs = [
+    { img: imgMarg1 },
+    { img: imgMarg2 },
+    { img: imgMarg3 },
+    { img: imgMarg4 },
+    { img: imgMarg5 },
+    { img: imgMarg6 },
+    { img: imgMarg7 },
+  ]
   return (
     <div>
       {/* banner */}
@@ -112,17 +128,54 @@ const Home = () => {
       {/* top destination for asia */}
       <section className='py-10'>
         <h2 className='mb-6 text-[28px] text-center font-medium w-full lg:text-4xl lg:w-3xl lg:mx-auto'>Top Destinations for Asia</h2>
-        <div>
+        <div className='flex flex-col gap-6 md:grid md:grid-cols-2 lg:grid-cols-3'>
           {destinations.map((destination, index) => (
-            <div key={index} className={`bg-[url(${destination.image}] bg-cover bg-center h-[468.48px]`}>
-              <div>
+            <div key={index} className="bg-cover bg-center h-[468.48px] bg-image p-6 flex flex-col justify-between" style={{ backgroundImage: `url(${destination.image})` }}>
+              <div className='flex items-center gap-3'>
                 <img src={destination.flag} alt={destination.name} />
-                <p>{destination.name}</p>
+                <p className='text-xl font-medium capitalize'>{destination.name}</p>
               </div>
-              <p>{destination.description}</p>
+              <p className='text-[2C2C2C] font-medium tracking-[0.08px]'>{destination.description}</p>
             </div>
           ))}
         </div>
+      </section>
+
+      {/* meets technology */}
+      <section className='py-10 text-center'>
+        <h1 className='mt-4 mb-6 text-[28px] font-medium w-full lg:text-5xl lg:w-3xl lg:mx-auto'>Where Compassion Meets Technology</h1>
+        <p className='text-md text-[#666666] font-medium'>Here, we believe that healthcare is personal and global. We unite empathy with innovation so that:</p>
+        <div className='flex flex-col gap-5 mt-8 lg:grid lg:grid-cols-3'>
+          {technology.map((tech, index) => (
+            <div key={index} style={{ backgroundImage: `url(${tech.img})` }} className='h-[280px] bg-cover bg-center bg-no-repeat rounded-[10px] p-6 lg:h-[411.53px]'>
+              <div className='text-start text-[18px] text-white font-medium'>{tech.title}</div>
+            </div>
+          ))}
+        </div>
+      </section>
+
+      {/* join us */}
+      <section className='py-10 flex flex-col gap-6 lg:flex-row'>
+        <p className='uppercase lg:w-1/3'>Join Us</p>
+        <div className='flex flex-col gap-4 lg:gap-8'>
+          <h3 className='text-[28px] font-medium leading-[normal] lg:text-5xl'>Together, Let's Redefine the Future of Healthcare Without Borders.</h3>
+          <p className='text-md text-[#666666] font-medium'>Join Medifly as we work to reduce barriers, expand choices, and bring world‑class healthcare within reach — everywhere, for everyone.</p>
+          <Button variant="custom" className="py-[16px] px-[32px] h-[50px] w-fit">
+            Partner with us
+            <FaArrowRightLong />
+          </Button>
+        </div>
+      </section>
+
+      {/* marquee */}
+      <section className='py-10'>
+        <Marquee>
+          <div className='flex flex-row gap-5'>
+            {imgMargs.map((imgMarg, index) => (
+              <img key={index} src={imgMarg.img} alt='image' className='rounded-[10px]'/>
+            ))}
+          </div>
+        </Marquee>
       </section>
     </div>
   )
